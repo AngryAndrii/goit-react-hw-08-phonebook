c2e45e89fdf0f12ec431b5a68670fa9e8117e685
AngryAndrii <121435378+AngryAndrii@users.noreply.github.com>
2023-10-06 13:51:13 +0000

    Deploying to gh-pages from @ AngryAndrii/goit-react-hw-08-phonebook@09a311aeccc6b81cd09b3d6d35a100ae80a7431e 🚀

---
+++ static/js/114.60994def.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[114],{114:function(e,n,t){t.r(n);var h=t(184);n.default=function(){return(0,h.jsx)(h.Fragment,{children:(0,h.jsx)("h3",{children:"it is home page"})})}}}]);
+//# sourceMappingURL=114.60994def.chunk.js.map
